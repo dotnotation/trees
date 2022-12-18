b5fc0d4ba3805ba5b88cc86b20fdfac16b469bb1
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-18 14:51:45 -0500

    added pseudocode

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -10,4 +10,19 @@ class BinarySearchTree {
     constructor(){
         this.root = null
     }
+
+    insert(val){
+        // create a new node
+        // start at root
+        // if no root, make root new node
+        // if there is a root, check if the value is greater than or less than
+        // if greater
+            // check to see if there is a node to the right
+                // if yes, move to that node and repeat
+                // if no, add the node as the right property
+        // if less
+            // check to see if there is a node to the left
+                // if yes, move to that node and repeat
+                // if no, add the new node as the left property
+    }
 }
