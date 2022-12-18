a6ea049ed117b8285c8ef505e2589fe99e3a7343
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-18 16:30:08 -0500

    setting up classes, constructors, and breadth first search function

--- tree_traversal.js
+++ tree_traversal.js
@@ -1,1 +1,34 @@
 // this is for traversing any tree, not just sorted binary search trees
+class Node {
+    constructor(val){
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+class TreeTraversal {
+    constructor(){
+        this.root = null
+    }
+
+    bfs(){
+        // breadth first search
+        // traversing tree horizontally aka visit all siblings before moving onto children
+        let visited = []
+        let queue = []
+        let node = this.root
+
+        queue.push(node)
+
+        while(queue.length){
+            // while there is something in the queue
+            // remove something from the beginning of the queue 
+            node = queue.shift()
+            visited.push(node.val)
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
+        }
+        return visited
+    }
+}
